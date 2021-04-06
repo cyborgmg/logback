624b0ca6660fc19fce6f3681d8fa2e6abc145e3a
cyborgmg <cyborgmg@gmail.com>
2021-04-06 12:43:22 -0300

    first commit

---
+++ src/main/java/com/cyborg/logback/LogbackApplication.java
@@ -0,0 +1,21 @@
+package com.cyborg.logback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Slf4j
+@SpringBootApplication
+public class LogbackApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("teste");
+	}
+}
